c730eae2585182b6ff9b9c9e879421f7c6140bf8
guilherminhu <guilhermeapbarbosa1@gmail.com>
2026-04-14 19:42:06 -0400

    primeiro commit 14/04

---
+++ projeto/treinos/navbar.js
@@ -0,0 +1,40 @@
+class mobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleClick() {
+console.log("this");
+        this.navList.classList.toggle(this.activeClass);
+this.mobileMenu.classList.toggle(this.activeClass);
+
+    }
+
+
+    addClickEvent = () => {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+ const mobileNavbar = new mobileNavbar(
+        ".mobile-menu",
+        ".nav-list",
+        ".nav-list li",
+    );
+
+mobileNavbar.init();
+
